323417723f1a8d20123cd78e2875d768b09a8cb8
jie.huang <slamdunk>
2021-08-01 22:33:49 +0800

    更新提醒逻辑完善

--- proxy.js
+++ proxy.js
@@ -1,44 +1,54 @@
-"use strict";
+"use strict"
 
-const { createClient, segment} = require("oicq");
-const uin = 631676243;
+const { createClient, segment} = require("oicq")
+const uin = 631676243
 const bot = createClient(uin)
 
-let TEST_GROUP_ID = "647415772";
+let isOnline = false
+let TEST_GROUP_ID = "647415772"
 
 function online() {
     bot.on("system", (data) => {
-        console.log("system event: " + data.system_type);
-    });
+        console.log("system event: " + data.system_type)
+    })
     bot.on("system.login.slider", () => {
         process.stdin.once("data", (input) => {
-            bot.sliderLogin(input);
-        });
-    });
+            bot.sliderLogin(input)
+        })
+    })
     bot.on("system.online", () => {
-        console.log("上线了！");
-        console.log(bot.getGroupList().data);
+        console.log("上线了！")
+        console.log(bot.getGroupList().data)
+        isOnline = true
         share("突然出现~", false)
-    });
+    })
     bot.on("system.offline", () => {
-        console.log("下线了！");
-        // console.log("重试上线...");
-        // bot.login("23dd23b8cf68e175421e9bf7bf97315a");
-    });
+        console.log("下线了！")
+        isOnline = false
+        // console.log("重试上线...")
+        // bot.login("23dd23b8cf68e175421e9bf7bf97315a")
+    })
     bot.on("message.private", (data) => {
-        console.log("msg.private: " + data.raw_message);
-        data.reply("hello, i'm a robot.");
+        console.log("msg.private: " + data.raw_message)
+        data.reply("hello, i'm a robot.")
     })
     bot.on("message.group", (data) => {
-        console.log("msg.group; id: " + data.group_id + "; msg: " + data.raw_message);
-        data.reply("hello, i'm a robot.");
+        console.log("msg.group; id: " + data.group_id + "; msg: " + data.raw_message)
+        data.reply("hello, i'm a robot.")
     })
 
     bot.login("23dd23b8cf68e175421e9bf7bf97315a")//mm:123456
 }
 
+function botOnline() {
+    return isOnline
+}
+
 function share(message, atAll) {
-    console.log(message)
+    console.log("share: " + message + "\nisOnline: " + isOnline)
+    if (!isOnline) {
+        return
+    }
     if (atAll) {
         message = "@全体成员 " + message
     }
@@ -48,6 +58,7 @@ function share(message, atAll) {
 
 module.exports = {
     online: online,
+    botOnline: botOnline,
     share: share
 }
 
--- task.js
+++ task.js
@@ -3,6 +3,7 @@
 const https = require("https")
 const schedule = require('node-schedule')
 const proxy = require("./proxy")
+const fs = require("fs")
 
 let URL_LIVE = "https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuMedalAnchorInfo?ruid=8060090"
 let JOB_LIVE = "job_live"
@@ -11,10 +12,12 @@ let jobLiveCancel = false
 let URL_VIDEO = "https://api.bilibili.com/x/space/arc/search?mid=8060090&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp"
 let JOB_VIDEO = "job_video"
 let jobVideoCancel = false
+let lastUpdateVid = ""
 
 let URL_RECORD = "https://api.bilibili.com/x/space/channel/video?mid=8060090&cid=31244&pn=1&ps=30&order=0&ctype=1&jsonp=jsonp"
 let JOB_RECORD = "job_record"
 let jobRecordCancel = false
+let yesterdayRecord = ""
 
 function checkLive() {
     https.get(URL_LIVE, (resp) => {
@@ -25,24 +28,71 @@ function checkLive() {
         resp.on('end', () => {
             let json = JSON.parse(data)
             if (json.data.live_stream_status === 1) {
-                console.log(json)
+                console.log("checkLive: " + json)
                 stopLiveJob()
                 proxy.share("开播啦\nhttps://live.bilibili.com/915663", true)
             } else {
-                console.log("还在拔腿毛，请稍等")
+                console.log("还在拔腿毛，请稍等~")
             }
         })
     }).on("error", (err) => {
-        console.log("Error: " + err.message)
+        console.error("checkLive error: " + err.message)
     })
 }
 
 function checkUpdate() {
-
+    https.get(URL_VIDEO, (resp) => {
+        let data = ''
+        resp.on('data', (chunk) => {
+            data += chunk
+        })
+        resp.on('end', () => {
+            let json = JSON.parse(data)
+            let vlist = json.data.list.vlist
+            let bvid = vlist[0].bvid
+            console.log("checkUpdate: " + vlist[0])
+            if (lastUpdateVid !== bvid) {
+                lastUpdateVid = bvid
+                fs.writeFile('./data/lastVid', bvid, function (err) {
+                    if (err) {
+                        console.error("writeFile error: " + err)
+                    } else {
+                        console.log("writeFile success")
+                    }
+                })
+                proxy.share("求三连[爱心]\nhttps://www.bilibili.com/video/" + bvid, true)
+            } else {
+                console.log("up主还未更新~")
+            }
+        })
+    }).on("error", (err) => {
+        console.error("checkUpdate error: " + err.message)
+    })
 }
 
 function checkRecord() {
-
+    https.get(URL_RECORD, (resp) => {
+        let data = ''
+        resp.on('data', (chunk) => {
+            data += chunk
+        })
+        resp.on('end', () => {
+            let json = JSON.parse(data)
+            let archives = json.data.list.archives
+            let pubdate = archives[0].pubdate
+            let ctime = archives[0].ctime
+            let bvid = archives[0].bvid
+            let title = archives[0].title
+            console.log("checkRecord: " + pubdate + ";" + ctime)
+            if (true) {
+                proxy.share(title + "\nhttps://www.bilibili.com/video/" + bvid, true)
+            } else {
+                console.log("up主昨天休息~")
+            }
+        })
+    }).on("error", (err) => {
+        console.error("checkRecord error: " + err.message)
+    })
 }
 
 function scheduleLiveJob() {
@@ -60,8 +110,9 @@ function stopLiveJob() {
 
 function scheduleVideoJob() {
     jobVideoCancel = false
-    schedule.scheduleJob(JOB_VIDEO, '0 0/1 * * * *', ()=>{
+    schedule.scheduleJob(JOB_VIDEO, '0 0/1 0,1,2,9-23 * * *', ()=>{
         console.log("checkUpdate: " + new Date())
+        checkUpdate()
     })
 }
 
@@ -74,6 +125,7 @@ function scheduleRecordJob() {
     jobRecordCancel = false
     schedule.scheduleJob(JOB_VIDEO, '0 0/1 * * * *', ()=>{
         console.log("checkRecord: " + new Date())
+        checkRecord()
     })
 }
 
@@ -84,13 +136,22 @@ function stopRecordJob() {
 
 function start() {
     scheduleLiveJob()
-    // scheduleVideoJob()
+    scheduleVideoJob()
     // scheduleRecordJob()
 
     schedule.scheduleJob("job_live_reset", '30 7 2 * * *', ()=>{
         console.log("job_live_reset: " + jobLiveCancel)
         if (jobLiveCancel) {
             scheduleLiveJob()
+        }
+    })
+
+    fs.readFile('./data/lastVid', function (err, data) {
+        if (err) {
+            console.error("readFile error: " + err);
+        } else {
+            console.log("readFile lastVid: " + data);
+            lastUpdateVid = data;
         }
     })
 }
