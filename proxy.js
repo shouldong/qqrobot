bd1f69cfed3844bdd032e16f21c23e1cb29f4af2
jie.huang <slamdunk>
2021-08-04 19:40:46 +0800

    完善回复逻辑

--- proxy.js
+++ proxy.js
@@ -1,11 +1,13 @@
 "use strict"
 
-const { createClient, segment} = require("oicq")
+const { createClient } = require("oicq")
 const uin = 631676243
 const bot = createClient(uin)
+const text = require('./text')
 
 let isOnline = false
-let TEST_GROUP_ID = "647415772"
+let TEST_GROUP_ID = 647415772
+let WORK_GROUP_ID = 1104280840
 
 function online() {
     bot.on("system", (data) => {
@@ -18,9 +20,11 @@ function online() {
     })
     bot.on("system.online", () => {
         console.log("上线了！")
-        console.log(bot.getGroupList().data)
+        for (let group of bot.getGroupList().data.values()) {
+            console.log("my group: " + group.group_name + "; " + group.group_id)
+        }
         isOnline = true
-        share("突然出现~", false)
+        // share("突然出现~", false)
     })
     bot.on("system.offline", () => {
         console.log("下线了！")
@@ -30,11 +34,22 @@ function online() {
     })
     bot.on("message.private", (data) => {
         console.log("msg.private: " + data.raw_message)
-        data.reply("hello, i'm a robot.")
+        // let reply = text.autoReply(data.raw_message)
+        // console.log("msg.reply: " + reply)
+        // if (reply) {
+        //     let result = data.reply(reply)
+        //     console.log("sendGroupMsg; " + result.retcode + "; " + result.status)
+        // }
     })
     bot.on("message.group", (data) => {
         console.log("msg.group; id: " + data.group_id + "; msg: " + data.raw_message)
-        data.reply("hello, i'm a robot.")
+        if (data.group_id === WORK_GROUP_ID) {
+            console.log("msg.group be target group")
+            let reply = text.autoReply(data.raw_message)
+            console.log("msg.reply: " + reply)
+            let result = data.reply(reply)
+            console.log("sendGroupMsg; " + result.retcode + "; " + result.status)
+        }
     })
 
     bot.login("23dd23b8cf68e175421e9bf7bf97315a")//mm:123456
@@ -52,7 +67,7 @@ function share(message, atAll) {
     if (atAll) {
         message = "@全体成员 " + message
     }
-    let result = bot.sendGroupMsg(TEST_GROUP_ID, message)
+    let result = bot.sendGroupMsg(WORK_GROUP_ID, message)
     console.log("sendGroupMsg; " + result.retcode + "; " + result.status)
 }
 
--- text.js
+++ text.js
@@ -1,11 +1,41 @@
-"use strict";
-var tell_joke = require('./joke');
+"use strict"
 
-function process_text(content) {
-    if (content.search('笑话') !== -1) {
-        return tell_joke();
+function autoReply(content) {
+    console.log("autoReply: " + content)
+    if (isBeAt(content)) {
+        console.log("autoReply be at")
+        let replay = dispatchContent(content)
+        if (replay) {
+            return replay
+        } else {
+            return "你講咩嘢？I'm just a robot."
+        }
     }
-    return "hello, i'm a robot.";
+    return null
 }
 
-export default process_text;
+function isBeAt(content) {
+    return content.search('CQ:at,qq=631676243,text=@我是机器瓜') !== -1;
+}
+
+
+function dispatchContent(content) {
+    console.log("dispatchContent: " + content)
+    if (content.search('笑话') !== -1 || content === '笑话') {
+        console.log("dispatchContent tellJoke")
+        return tellJoke()
+    }
+    return null
+}
+
+function tellJoke() {
+    return JOKE_ARR[Math.floor(Math.random() * JOKE_ARR.length)]
+}
+
+let JOKE_ARR = [
+    "有把游戏，关羽狂送人头。\n队友打字问：关羽可以猥琐一点吗？\n关羽沉默一会，公屏打字问：对面的咬金弟弟一起睡觉吗嘿嘿嘿。",
+    "法国小哥最近在跟我学中文。\n有天我和他聊天时发了句呵呵，23333，他问我什么意思，我说就是夸你超好笑的意思。\n今天和他一起见中国朋友，朋友说完话后，法国小哥口齿清晰又标准的发出一句\"呵呵，两万三千三百三十三。\"全场死一般沉寂。"]
+
+module.exports = {
+    autoReply: autoReply
+}
